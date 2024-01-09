e9cbfe320afd388883c3ebb25e494332fa49e33b
alvi101 <129574736+alvi101@users.noreply.github.com>
2024-01-09 19:15:03 +0600

    Django Module 15.5 Practice Day code - Alvi Ahmed

---
+++ musician_directory/album/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from musicians.models import Musician
+
+# Create your models here.
+
+
+class Album(models.Model):
+    album_name = models.CharField(max_length=200)
+    release_date = models.DateField()
+    rating = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    ]
+    album_rating = models.CharField(max_length=6, choices=rating)
+    musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.album_name
